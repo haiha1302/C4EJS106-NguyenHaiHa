e24eb7c389b5dc54a84d19991b671654782539b7
Nguyen Hai Ha <td12a11haiha@gmail.com>
2021-04-17 22:02:41 +0700

    update bai test lan 1

--- test cuoi khoa/B/b.js
+++ test cuoi khoa/B/b.js
@@ -3,8 +3,16 @@ var btn = document.getElementById('btn')
 
 btn.addEventListener('click', randomNumber)
 
+// function click() {
+//     var a = 0
+//     a += 1
+//     console.log(a);
+// }
+
 function randomNumber(num) {
     var randomNumber = Math.floor(Math.random() * 10) + 1
+    var a = 0
+    a += 1
     if(inputNumber == randomNumber) {
         alert(`Chúc mừng bạn đã nhập đúng!!!`)
     }
